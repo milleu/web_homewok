87761e6516ce73e256b34893af8d477229b88fa5
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-10-08 14:24:10 -0700

    all settings added

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from main.models import Product, Category
+
+# Register your models here.
+
+
+#admin.site.register(Student)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('product_name', 'description')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    description = models.TextField( verbose_name='Описание')
+    avatar = models.ImageField(upload_to='products/', verbose_name='Превью', null=True, blank=True)
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    date_of_creation = models.DateTimeField(verbose_name='Дата создания')
+    date_of_last_chanage = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+
+    def __str__(self):
+        return f'{self.product_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
+
+class Category(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    description = models.TextField( verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.product_name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('product_name',)
