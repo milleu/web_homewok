aded94bd0c01ae161831affbb9a6f4fedbfab947
Daria Beliaeva <dariabeliaeva22@gmail.com>
2023-10-15 12:53:14 -0700

    all settings added

--- main/admin.py
+++ main/admin.py
@@ -3,12 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from main.models import Product, Category
+from main.models import Student
 
 # Register your models here.
 
 
-#admin.site.register(Student)
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('product_name', 'description')
+admin.site.register(Student)
--- main/models.py
+++ main/models.py
@@ -1,35 +1,22 @@
 from django.db import models
 
+import main.models
+
 # Create your models here.
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Product(models.Model):
-    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
-    description = models.TextField( verbose_name='Описание')
-    avatar = models.ImageField(upload_to='products/', verbose_name='Превью', null=True, blank=True)
-    category = models.CharField(max_length=100, verbose_name='Категория')
-    price = models.IntegerField(verbose_name='Цена')
-    date_of_creation = models.DateTimeField(verbose_name='Дата создания')
-    date_of_last_chanage = models.DateTimeField(verbose_name='Дата последнего изменения')
+class Student(models.Model):
+    first_name = models.CharField(max_length=100, verbose_name='имя')
+    last_name = models.CharField(max_length=100, verbose_name='фамилия')
+    avatar = models.ImageField(upload_to='students/', verbose_name='аватар', null=True, blank=True)
 
+    is_active = models.BooleanField(default=True, verbose_name='учится')
 
     def __str__(self):
-        return f'{self.product_name} {self.description}'
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ('product_name',)
-
-class Category(models.Model):
-    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
-    description = models.TextField( verbose_name='Описание')
-
-    def __str__(self):
-        return f'{self.product_name} {self.description}'
-
-    class Meta:
-        verbose_name = 'Категория'
-        verbose_name_plural = 'Категории'
-        ordering = ('product_name',)
+        verbose_name = 'студент'
+        verbose_name_plural = 'студенты'
+        ordering = ('last_name',)
